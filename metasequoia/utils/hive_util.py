941774eff4031c1a5c69e459b656be5e491781d3
changxing <1278729001@qq.com>
2024-03-15 13:38:28 +0800

    将 hive 工具类的 execute 拆分出是否执行 fetch 的两个

--- metasequoia/utils/hive_util.py
+++ metasequoia/utils/hive_util.py
@@ -5,6 +5,14 @@ def execute(hive_instance: HiveInstance, sql: str):
     """执行 Hive 语句"""
     with HiveConn(hive_instance) as conn:
         with conn.cursor() as cursor:
+            result = cursor.execute(sql)
+            return result
+
+
+def execute_and_fetch_result(hive_instance: HiveInstance, sql: str):
+    """执行 Hive 语句"""
+    with HiveConn(hive_instance) as conn:
+        with conn.cursor() as cursor:
             cursor.execute(sql)
             result = cursor.fetchall()
             return result
