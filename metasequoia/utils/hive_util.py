8c12e6ec0c4983e67c8dfc769e77d3cdddedacab
changxing <1278729001@qq.com>
2024-04-12 19:57:29 +0800

    新增 Hive 部分工具类

--- metasequoia/utils/hive_util.py
+++ metasequoia/utils/hive_util.py
@@ -1,3 +1,5 @@
+from pyhive import hive
+
 from metasequoia.connector.hive_connector import HiveInstance, HiveConn
 
 
@@ -16,3 +18,28 @@ def execute_and_fetch_result(hive_instance: HiveInstance, sql: str):
             cursor.execute(sql)
             result = cursor.fetchall()
             return result
+
+
+def conn_execute(hive_conn: hive.Connection, sql: str):
+    """执行 Hive 语句"""
+    with hive_conn.cursor() as cursor:
+        result = cursor.execute(sql)
+        return result
+
+
+def conn_execute_and_fetch_result(hive_conn: hive.Connection, sql: str):
+    """执行 Hive 语句"""
+    with hive_conn.cursor() as cursor:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+
+
+def conn_get_create_table(hive_conn: hive.Connection, table_name: str):
+    """获取建表语句"""
+    with hive_conn.cursor() as cursor:
+        cursor.execute(f"SHOW CREATE TABLE {table_name}")
+        result = []
+        for row in cursor.fetchall():
+            result.append(row[0])
+        return "\n".join(result)
