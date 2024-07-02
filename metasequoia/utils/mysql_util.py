4002be913c0e87d3de9d7273a1a4e4e84d38a51c
changxing <1278729001@qq.com>
2024-07-02 15:50:51 +0800

    提交迁移的 metasequoia 的 MySQL 工具类

--- metasequoia/utils/mysql_util.py
+++ metasequoia/utils/mysql_util.py
@@ -7,22 +7,3 @@ from typing import Tuple, Dict, Any
 import pymysql
 import pymysql.cursors
 
-
-def conn_select_sql_as_dict(conn: pymysql.Connection, sql: str) -> Tuple[Dict[str, Any], ...]:
-    """通过 MySQL 根据 WHERE 条件抽取数据
-
-    Parameters
-    ----------
-    conn : pymysql.Connection
-        Mysql 连接
-    sql : str
-        SQL 语句
-
-    Returns
-    -------
-    List[Dict[str, Any]]
-        根据 sql 和 query_data 读取的数据
-    """
-    with conn.cursor(pymysql.cursors.DictCursor) as cursor:
-        cursor.execute(sql)
-        return cursor.fetchall()
